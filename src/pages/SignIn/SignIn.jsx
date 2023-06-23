b3856a8adba03a019e5f745c6d4a280c36427e9f
ManausK <1905246@kiit.ac.in>
2023-06-23 10:44:56 +0530

    initial commit

---
+++ src/pages/SignIn/SignIn.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SignIn = () => {
+  return (
+    <div className="signIn">
+      <div className="heading">
+        <h1>Welcome Back</h1>
+        <p>Sign in to your account</p>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
